93906caa93142851454dc09d86dd64c2ef065486
AlekseyYaramov <aleksei1990@abv.bg>
2017-06-28 12:25:30 +0300

    Popup validation
    Popup validation

--- web/scripts/main.js
+++ web/scripts/main.js
@@ -31,3 +31,76 @@ $(document).ready(function() {
 		return false;
 	});
 });
+
+
+/* popup for button reject */
+
+var popup = document.getElementById('mypopup');
+var btn = document.getElementById("reject");
+var span = document.getElementsByClassName("close")[0];
+var button_cancel_popup = document.getElementById("button_cancel_popup");
+var button_submit_popup = document.getElementById("button_submit_popup");
+								
+btn.onclick = function() {
+	popup.style.display = "block";
+}
+button_cancel_popup.onclick = function() {
+	popup.style.display = "none";
+}
+
+span.onclick = function() {
+	popup.style.display = "none";
+}
+window.onclick = function(event) {
+if (event.target == popup) {
+	popup.style.display = "none";
+									}
+}
+
+
+
+function validate() {
+    var errorNode = this.parentNode.querySelector( ".error" ),
+        span = document.createElement( "span" )
+    
+    this.classList.remove( "invalid" );
+    if ( errorNode ) {
+        errorNode.parentNode.removeChild( errorNode );
+    }
+
+    if ( !this.validity.valid ) {
+        this.classList.add( "invalid" );
+        this.parentNode.appendChild( span );
+        span.classList.add( "error" );
+        span.innerHTML = this.getAttribute(
+        this.validity.valueMissing ? "data-required-message" : "data-type-message" );
+		button_submit_popup.onclick = function() {
+	    popup.style.display = "none";
+}
+    }
+};
+
+var form = document.querySelector( "form" ),
+    inputs = form.querySelectorAll( "input" );
+
+for ( var i = 0; i < inputs.length; i++ ) {
+    inputs[ i ].addEventListener( "blur", validate );
+    inputs[ i ].addEventListener( "invalid", validate );
+};
+
+// Turn off the bubbles
+form.addEventListener( "invalid", function( event ) {
+    event.preventDefault();
+}, true );
+
+// Support: Safari, iOS Safari, default Android browser
+document.querySelector( "form" ).addEventListener( "submit", function( event ) {
+    if ( this.checkValidity() ) {
+        alert( "Successful submission" );
+    } else {
+        event.preventDefault();
+    }
+});
+
+
+
